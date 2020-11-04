08b0148e63a954b1077acc9bcf62f30c711903a0
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-11-05 10:29:07 +1300

    Saving progress

--- src/index.js
+++ src/index.js
@@ -94,3 +94,120 @@ function drawPaddle() {
     ctx.fill();
     ctx.closePath();
 }
+
+// Draw the score board
+function drawScore() {
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
+}
+
+// Draw bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => { // For columns
+        column.forEach(brick => {
+            ctx.beginPath();
+            ctx.rect(brick.x, brick.y, brick.w, brick.h);
+            ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent'; // This will be conditional
+            ctx.fill();
+            ctx.closePath();
+        });
+    });
+}
+
+// REVIEW Move paddle on canvas
+function movePaddle() { //Every time draw on the canvas with can re-draw with certain element
+    paddle.x += paddle.dx; // Paddle will not move until we use the keyboards events
+
+    // Wall detection
+    if (paddle.x + paddle.w > canvas.width) { // Entire width of the canvas
+        paddle.x = canvas.width - paddle.w; // Minus the paddle width
+    }
+
+    if (paddle.x < 0) { // 0 from the x-axes and this is for the borders detection
+        paddle.x = 0;
+    }
+}
+
+// Move ball on canvas
+function moveBall() {
+    ball.x += ball.dx; // Append the ball on the x-axes
+    ball.y += ball.dy; // Append the ball on the y-axes
+
+    // Wall collision (right/left)
+    if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0) { // Right and left walls
+        ball.dx *= -1; // ball.dx = ball.dx * -1 the reason we are doing this is to reverse the ball to go the other way and bounce back
+    }
+
+    // Wall collision (top/bottom)
+    if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) { // Top and bottom walls
+        ball.dy *= -1; // The reason we are doing this is to reverse the ball to go to the other wall and bounce back
+    }
+
+    // console.log(ball.x, ball.y);
+
+    // Paddle collision
+    if (
+        ball.x - ball.size > paddle.x && // Always to in consideration the object size of ball and this check
+        ball.x + ball.size < paddle.x + paddle.w && // Checking the right side
+        ball.y + ball.size > paddle.y 
+    ) {
+        ball.dy = -ball.speed; // Reverse the ball object and bounce off
+    }
+
+    // Brick collision
+    bricks.forEach(column => { // Loop through bricks array
+        column.forEach(brick = > {
+            if (brick.visible) { // Make sure the bricks are visible
+                if (
+                    ball.x - ball.size > brick.x && // Left brick side check
+                    ball.x + ball.size < brick.x + brick.w && // Right brick side check
+                    ball.y + ball.size > brick.y && // Top brick side check
+                    ball.y - ball.size < brick.y + brick.h // Bottom brick side check
+                ) {
+                    ball.dy *= -1; // Bounce off the brick
+                    brick.visible = false; // Once bounce off the brick make it visible false
+                    increaseScore(); // Increase score
+                }
+            }
+        });
+    });
+
+    // Hit bottom wall - lose
+    if (ball.y + ball.size > canvas.height) { // If hit bottom wall then lose
+        showAllBricks(); // Redraw bricks
+        score = 0; // Reset score
+    }
+}
+
+// Increase Score
+function increaseScore() {
+    score++; // Increment by one
+
+    if (score % (brickRowCount * brickRowCount) === 0) { // Check the if there are no bricks if true redraw
+        showAllBricks(); // Create brick wall
+    }
+}
+
+// Make all bricks appear
+function showAllBricks() { // Loop through the array of bricks
+    bricks.forEach(column => { // For column set visible to true
+        column.forEach(brick => (brick.visible = true));
+    });
+}
+
+// REVIEW Setup all the drawing
+function draw() {
+    // Clear the canvas to make sure ends the event once stop using the keys
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    drawBall()
+    drawPaddle()
+    drawScore()
+    drawBricks()
+}
+
+// Updat ecanvas drawing and animation
+function updates() {
+    movePaddle();
+    moveBall();
+}
+
